e9c21a848acbf435a07dc9ab0e4aacc8abaf7bcd
f1ooow <f1ooow1999@gmail.com>
2025-12-30 14:41:03 +0800

    fix: 忽略 pdfjs canvas 依赖

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    // 忽略 pdfjs-dist 的 canvas 依赖（浏览器中使用原生 canvas）
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
