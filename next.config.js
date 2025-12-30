7e834c918c9f902be6cf44d1ea37612f38a1d151
f1ooow <f1ooow1999@gmail.com>
2025-12-30 14:37:37 +0800

    fix: 移除 standalone 输出模式

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 允许外部图片
   images: {
     unoptimized: true,
   },
-  // 禁用静态导出时的图片优化
-  output: 'standalone',
 };
 
 module.exports = nextConfig;
